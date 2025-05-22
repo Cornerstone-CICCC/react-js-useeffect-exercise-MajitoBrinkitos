0406d1b967b4e96e4eac11f8760e369d4eba939b
Majito Brinkitos <mcvef2013@gmail.com>
2025-05-22 12:17:14 -0700

    useEffect complete

--- effect-on-state/src/App.tsx
+++ effect-on-state/src/App.tsx
@@ -13,10 +13,32 @@ function App() {
     }
   }, [message])
 
+  {/*Message Logger Handler */}
   const sendMessage = () =>{
     alert('Hello from React!')
   }
 
+  {/*Likes Counter */}
+  useEffect(() => {
+    if(likes > 0) {
+      alert(`Likes update to ${likes}`)
+    }
+  }, [likes])
+
+  {/*Likes Counter Handler */}
+  const updateLikes = () => {
+    setLikes((prevLikes) => prevLikes + 1)
+  }
+
+  {/*Color Box */}
+  useEffect(() => {
+    console.log(`Current color: ${color}`)
+  }, [color])
+
+  {/*Color Box Handler */}
+  const toggleColor = () => {
+    setColor((prevColor) => (prevColor === 'red' ? 'blue' : 'red'))
+  }
 
     return (
       <>
@@ -29,6 +51,35 @@ function App() {
               onClick={sendMessage}>
                 Send Message
               </button>
+          </div>
+
+          {/*Likes Counter */}
+          <div>
+            <h2>Likes Counter</h2>
+            <button
+            type='button'
+            onClick={updateLikes}>
+              Likes
+            </button>
+          </div>
+
+          {/*Color Box */}
+          <div>
+            <h2>Color Box</h2>
+            <button
+            type='button'
+            onClick={toggleColor}>
+              Change Color
+            </button>
+            <div style={{
+              width: '100px',
+              height: '100px',
+              backgroundColor: color,
+              display:'flex',
+              justifyContent: 'center',
+            }}>
+              {color}
+            </div>
           </div>
         </div>
       </>
